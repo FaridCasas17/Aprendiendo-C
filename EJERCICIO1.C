f8ae65bf957fb7a35491f0c2efc068a4db03257f
Farid Casas <faridstaward2005@gmail.com>
2026-04-30 21:48:03 -0500

    commit impresion de los numeros del 1er parametro hasta el segundo

--- EJERCICIO1.C
+++ EJERCICIO1.C
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(void) {
-    int a, b;
+    int a, b, i;
     int suma, resta, multiplicacion, residuo;
     float division;
     double potencia;
@@ -14,14 +14,30 @@ int main(void) {
 
 
     // COMPARACION DE NUMEROS:
+
+    // A mayor que B
     if (a > b){
-        printf("\n A es mayor que B");
+        printf("\n %d es mayor que %d",a,b);
+        //IMPRESION DE LOS NUMEROS DE A HASTA B
+        printf("\n Numeros desde %d hasta %d :\n",a,b);   
+
+        //Decremento A hasta B
+        for (i = a; i >= b; i--){
+        printf("%d ", i);
+        }
+    
     }
     else if(a < b){
-        printf("\n A es menor que B");
+        printf("\n %d es menor que %d",a,b);
+        printf("\n Numeros desde %d hasta %d :\n",a,b);   
+
+        //Incremento de A hasta B
+        for(i=a; i<=b; i++){
+            printf("%d ",i);
+        }
     }
     else {
-        printf("\nA y B son iguales");
+        printf("\n%d y %d son iguales",a,b);
     }
 
 
@@ -32,21 +48,22 @@ int main(void) {
     multiplicacion = a * b;
     potencia =  pow(a,b);
 
-//COMPARACION DE DIVISION Y RESIDUO SI B=0
-    if (b != 0) {
-        division = (float)a / b;
-        residuo = a % b;
-    } else {
-        printf("\nNo se puede dividir entre 0");
-        division = 0;
-        residuo = 0;
-    }
 
     printf("\n\n      OPERACIONES: \n");
     printf("\nSUMA: %d + %d = %d", a,b,suma);
     printf("\nRESTA: %d - %d = %d", a,b,resta);
     printf("\nMULTIPLIACION: %d x %d = %d", a,b,multiplicacion);
     printf("\nDIVISION: %d / %d = %.2f", a,b,division);
+   
+    //COMPARACION DE DIVISION Y RESIDUO SI B=0
+    if (b != 0) {
+        division = (float)a / b;
+        residuo = a % b;
+    } else {
+        printf("    No se puede dividir entre 0");
+        division = 0;
+        residuo = 0;
+    }
     printf("\nPOTENCIA: %d ^ %d = %.2lf",a,b,potencia);
     printf("\nRESIDUO: %d %% %d = %d",a,b,residuo);
     printf("\n");
